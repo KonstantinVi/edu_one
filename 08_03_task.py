4a9225a1f711d9cc7c8989f6bdc2a750f5c6fb70
Konstantin_Victorovich <kbrb1515@gmail.com>
2022-12-07 19:25:40 +0300

    08_03_task commit

---
+++ 08_03_task.py
@@ -0,0 +1,34 @@
+''' Chapter 8
+Programming exercises №02
+'''
+
+
+def main():
+    '''The main function of the module.'''
+
+    try:
+
+        user_data = input('Введите дату цифрами через обратный слеш: ')
+        data_mass = user_data.split('/')
+
+        month_system = ['Января', 'Февраля', 'Марта', 'Апреля',
+                        'Мая', 'Июня', 'Июля', 'Августа',
+                        'Сентября', 'Октября', 'Ноября', 'Декабря'
+                        ]
+
+        data_mass[1] = month_system[int(data_mass[1]) + 1]
+
+        print(f'{data_mass[0]} {data_mass[1]} {data_mass[2]} г.')
+
+    except IOError:
+        print('File not found')
+
+    except IndexError:
+        print('Indexing error')
+
+    except:
+        print('An error has occurred')
+
+
+if __name__ == '__main__':
+    main()
